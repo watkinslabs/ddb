6ef3e9cebbf4a8bf3d4c572c413fbc14c73556de
Charles Watkins <chris17453@gmail.com>
2020-06-13 14:05:45 -0400

    Commiting Build 1.1.501

--- source/parser/queries/select/queries.c
+++ source/parser/queries/select/queries.c
@@ -991,12 +991,13 @@ int validate_select(cursor_t * cursor,select_t *select){
                              break;
             
             break;
-            default:
+            //default:
                 
                 printf("UNKNOWN COLUMN TYPE IN SELECT: %s\n",token_type(tmp_ptr->type));
-                return 0;
+                //return 0;
         }
-        
+        printf("COLUMN TYPE IN SELECT FIXUP: %d-%s \n",tmp_ptr->ordinal,token_type(tmp_ptr->type));
+
         tmp_ptr=tmp_ptr->next;
     }
 
