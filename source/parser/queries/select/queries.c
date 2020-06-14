52a7d6b0e55bed2da881b142ea8ac345ed7fe1b5
Charles Watkins <chris17453@gmail.com>
2020-06-14 14:22:13 -0400

    Commiting Build 1.1.580

--- source/parser/queries/select/queries.c
+++ source/parser/queries/select/queries.c
@@ -40,6 +40,8 @@ int compare_identifiers(identifier_t *source,identifier_t *dest){
     if(dest  ==0) return 0;
     if(source->qualifier==0) return 0;
     if(dest  ->qualifier==0) return 0;
+    if(source->source   ==0) return 0;
+    if(dest  ->source   ==0) return 0;
     if (strcmp(source->qualifier,dest->qualifier)==0 && 
         strcmp(source->source,dest->source)==0) return 1;
     return 0;
