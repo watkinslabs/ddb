202d544989bd9d0db82371b3eef01322fad49372
Charles Watkins <chris17453@gmail.com>
2020-06-20 18:09:58 -0400

    Commiting Build 1.1.621

--- source/include/queries.h
+++ source/include/queries.h
@@ -27,7 +27,7 @@
     expression_t   * process_group_column_list(token_array_t *tokens,int *index);
     expression_t   * process_order_column_list(token_array_t *tokens,int *index);
     data_column_t    add_data_column          (data_column_t *column,unsigned int type,void *item,char *alias,int ordinal);
-    data_column_t    process_select_list      (token_array_t *tokens,int *index);
+    data_column_t  * process_select_list      (token_array_t *tokens,int *index);
     select_t       * process_select           (token_array_t *tokens,int *start);
     expression_t   * process_column_list      (token_array_t *tokens,int *index);
     table_def_t    * process_create_table     (token_array_t *tokens,int *start);
