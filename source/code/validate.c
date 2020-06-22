1d06b777f01832f60e5287cfccb75da8c3944fd1
Charles Watkins <chris17453@gmail.com>
2020-06-22 18:55:36 -0400

    Commiting Build 1.1.683

--- source/code/validate.c
+++ source/code/validate.c
@@ -322,9 +322,12 @@ int validate_select(cursor_t * cursor,select_t *select){
                 if(temp_ident->qualifier) {
                     // is it in the from?
                     table_def_t *temp_table=0;
+                    
                     if(strcmp(temp_ident->qualifier,select->alias)==0) {
+                        printf("CHECK FROM---\n");
                         temp_table=get_table_by_identifier(cursor,select->from);
                     } else {
+                        printf("CHECK JOIN---\n");
                         join_t *tmp_join=select->join;
                         int len=select->join_length;
                         for(int i=0;i<len;i++){
