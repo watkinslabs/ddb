acdaf4ce25058a168e033f04c6e8c079101f55c3
Charles Watkins <chris17453@gmail.com>
2020-06-22 18:50:44 -0400

    Commiting Build 1.1.680

--- source/code/validate.c
+++ source/code/validate.c
@@ -329,7 +329,7 @@ int validate_select(cursor_t * cursor,select_t *select){
                         int len=select->join_length;
                         for(int i=0;i<len;i++){
                             if(strcmp(tmp_join[i].alias,temp_ident->qualifier)==0){
-                                temp_table=get_table_by_identifier(cursor,select->from);
+                                temp_table=get_table_by_identifier(cursor,tmp_join[i].identifier);
                                 break;
                             }
                         }// end for
