075acd5fac98421a6212ffc32e062b5b2cb3bf39
Charles Watkins <chris17453@gmail.com>
2020-06-24 22:18:46 -0400

    Commiting Build 1.1.894

--- source/code/execute.c
+++ source/code/execute.c
@@ -130,7 +130,7 @@ row_t *build_row(char *data,range_t *range,char delimiter){
         ++row->column_length;
     }
     //printf("%d \n",row->column_length);
-    row->columns=safe_malloc(sizeof(char*),row->column_length);
+    row->columns=safe_malloc(sizeof(void*),row->column_length);
     //scan the row and duplicate the data into the columns
     in_block=0;
     int ordinal=0;
@@ -155,7 +155,7 @@ row_t *build_row(char *data,range_t *range,char delimiter){
                 //value[len]=0;
                 if(len>0) {
                     memcpy(value,&data[start_pos],len);
-                    printf (" %s \n",value);
+                  //  printf (" %s \n",value);
                 }
                 row->columns[ordinal]=*value;
             }
