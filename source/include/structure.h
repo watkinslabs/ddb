9e4158366d3c0bdb1f4a333fd77c9a4254ba16fa
Charles Watkins <chris17453@gmail.com>
2020-06-25 11:47:37 -0400

    Commiting Build 1.2.26

--- source/include/structure.h
+++ source/include/structure.h
@@ -135,6 +135,7 @@ typedef struct data_set_t{
     data_column_t * columns;
     long            column_length;
     long            row_length;
+    long            row_length2;
     row_t         * rows;
 } data_set_t;
 
