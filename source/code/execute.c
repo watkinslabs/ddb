71e8c30d6b438bc3a0b21b8f74c01c188b226e4f
Charles Watkins <chris17453@gmail.com>
2020-06-24 20:11:57 -0400

    Commiting Build 1.1.867

--- source/code/execute.c
+++ source/code/execute.c
@@ -209,7 +209,9 @@ int load_file(cursor_t *cursor,identifier_t *table_ident){
         while(range){
             printf("Range %ld-%ld\n",range->start,range->end);
             range=get_line(data,&position,fsize);
-            row_t *row=build_row(data,range,delimiter);
+            if(range) {
+                row_t *row=build_row(data,range,delimiter);
+            }
         }
 
             /*
