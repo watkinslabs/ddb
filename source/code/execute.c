724550c43bcd2f2275b8dfd31eaf1f03e0a4cb07
Charles Watkins <chris17453@gmail.com>
2020-06-24 22:26:30 -0400

    Commiting Build 1.1.916

--- source/code/execute.c
+++ source/code/execute.c
@@ -158,7 +158,7 @@ row_t *build_row(char *data,range_t *range,char delimiter){
                   //  printf (" %s \n",value);
                 }
                 row->columns[ordinal]=value;
-                printf (" %s \n",value);
+                //printf (" %s \n",value);
                 printf (" %s \n",row->columns[ordinal]);
             }
             ++ordinal;
