e0515231263d2edf9b7260f66681258dc8cf6a61
Charles Watkins <chris17453@gmail.com>
2020-06-25 14:21:41 -0400

    Commiting Build 1.2.51

--- source/code/execute.c
+++ source/code/execute.c
@@ -255,7 +255,7 @@ data_set_t *load_file(cursor_t *cursor,identifier_t *table_ident){
         }
 
             
-        //debug_dataset(data_set);
+        debug_dataset(data_set);
 
         free(data);
         return data_set;
