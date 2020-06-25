3a492c9e79af854575506d4b7ad1977b0349912f
Charles Watkins <chris17453@gmail.com>
2020-06-24 22:04:30 -0400

    Commiting Build 1.1.873

--- source/code/execute.c
+++ source/code/execute.c
@@ -147,9 +147,9 @@ row_t *build_row(char *data,range_t *range,char delimiter){
             }
             continue;
         }
-        printf("%c",data[pos]);
         if(data[pos]==',') {
             int len=start_pos-pos-1;
+            printf("%d",len);
             if(len>=0) {
                 char *value=safe_malloc(len+1,1);
                 if(len>0) {
