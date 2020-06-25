0e4aaf475ce1becb2900dcc48074f7f007d13cad
Charles Watkins <chris17453@gmail.com>
2020-06-24 22:46:27 -0400

    Commiting Build 1.1.972

--- source/code/debug.c
+++ source/code/debug.c
@@ -276,7 +276,7 @@ void debug_dataset(data_set_t *data){
         row_t *temp_row=&data->rows[i];
         printf("Length %d\n",temp_row->column_length);
         for(int b=0;temp_row->column_length;b++){
-            if(b!=0) printf(",");
+            //if(b!=0) printf(",");
             
            // if(&temp_row->columns[b]) {
            //     printf("%s",temp_row->columns[b]);
