769cf937e1862a6ac3d8104e30af23020a144882
Charles Watkins <chris17453@gmail.com>
2020-06-27 11:11:11 -0400

    Commiting Build 1.2.108

--- source/code/execute.c
+++ source/code/execute.c
@@ -99,7 +99,7 @@ int execute_select(cursor_t * cursor,select_t *select){
     //loop through from and joins
     // only add rows that pass WHERE, then JOIN ON
     int row_count=0;
-    //char ** columns=get_column_list(select->columns,select->column_length);
+    char ** columns=get_column_list(select->columns,select->column_length);
     //data_set_t *results=new_data_set(columns,select->column_length,row_count);
 
 
