d6de17c9c97d5e0e7a3d68d6b1e52cf499d94686
Charles Watkins <chris17453@gmail.com>
2020-06-27 16:41:49 -0400

    Commiting Build 1.2.123

--- source/include/duplicate.h
+++ source/include/duplicate.h
@@ -7,7 +7,7 @@
     identifier_t   * duplicate_identifier       (identifier_t *ident);
     char          ** duplicate_data_set_columns (char **columns,long length);
     row_t          * duplicate_dataset_row      (row_t *row);
-
+    data_set_t     * duplicate_data_set         (data_set_t *data_set);
     data_column_t  * duplicate_columns          (data_column_t *columns);
     table_def_t    * duplicate_table            (table_def_t *table);
     cursor_t       * duplicate_cursor           (cursor_t *cursor);
