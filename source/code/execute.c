a4d38f7cc6666d2f920c12393d20de8eb5a46ff3
Charles Watkins <chris17453@gmail.com>
2020-07-01 10:49:47 -0400

    Commiting Build 1.2.451

--- source/code/execute.c
+++ source/code/execute.c
@@ -540,27 +540,27 @@ int execute_select(cursor_t * cursor,select_t *select){
         int results=0;
         
 
-    // loop through WHERE
-    for(long i=0;i<data_sets[0]->row_length;i++){
-        results=evaluate_expressions(cursor,select->where);
-        // if where clause fails.. we skip this row
-        if(results==0) continue;
+        // loop through WHERE
+        for(long i=0;i<data_sets[0]->row_length;i++){
+            results=evaluate_expressions(cursor,select->where);
+            // if where clause fails.. we skip this row
+            if(results==0) continue;
 
-        // loop through JOIN
-        for(int set=1;set<data_set_count;set++){
-            for(long i=0;i<data_sets[set]->row_length;i++){
-                data_sets[set]->position=i;
-                expression_t *expressions=&select->join[set];
-                results=evaluate_expressions(cursor,expressions);
-                
-                
-                if(results) {
+            // loop through JOIN
+            for(int set=1;set<data_set_count;set++){
+                for(long i=0;i<data_sets[set]->row_length;i++){
+                    data_sets[set]->position=i;
+                    expression_t *expressions=&select->join[set];
+                    results=evaluate_expressions(cursor,expressions);
                     
+                    
+                    if(results) {
+
+                    }
+                    //else        printf("where expression false\n");
                 }
-                //else        printf("where expression false\n");
             }
         }
-
     }
     
     
