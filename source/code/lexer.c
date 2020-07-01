73af9c358a8cee2b29cbbbe3596b9989bd6adbc5
Charles Watkins <chris17453@gmail.com>
2020-06-30 23:47:16 -0400

    Commiting Build 1.2.417

--- source/code/lexer.c
+++ source/code/lexer.c
@@ -614,6 +614,7 @@ int process_queries(cursor_t *cursor,char *queries){
         tmp_ptr=tmp_ptr->next;
         free(tmp_ptr2);
     }
+    print_tokens(tokens);
     cursor->parse_position=tokens->position;
     tokens_destroy(tokens);
     clock_gettime(CLOCK_REALTIME,&cursor->ended);
