58d65b70a28bddf9129abfe2cca389cca0fb280b
Charles Watkins <chris17453@gmail.com>
2020-06-30 22:44:58 -0400

    Commiting Build 1.2.385

--- source/code/execute.c
+++ source/code/execute.c
@@ -396,6 +396,7 @@ expression_value_t *evaluate_expression(cursor_t *cursor,expression_t **expr){
 }
 
 int compare_expressions(cursor_t *cursor,expression_t **expr){
+    printf("compare\n");
     expression_value_t *exprV1=evaluate_expression(cursor,expr);
 
     debug_expression_value(exprV1);
