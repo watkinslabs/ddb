883466ac2eb1e502525d0162f8cfd09a17933af3
Charles Watkins <chris17453@gmail.com>
2020-07-01 13:46:12 -0400

    Commiting Build 1.2.484

--- source/code/execute.c
+++ source/code/execute.c
@@ -546,7 +546,7 @@ int execute_select(cursor_t * cursor,select_t *select){
         // FULL (OUTER) JOIN: Returns all records when there is a match in either left or right table
         
         for(int set=0;set<data_set_count;set++){
-            printf ("SET %d,\n"set);
+            printf ("SET %d\n",set);
             //if(set>0 && data_sets[set-1]->position==-2) continue;
             for(long i=0;i<data_sets[set]->row_length;i++){
                 data_sets[set]->position=i;
