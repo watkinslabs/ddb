e668792fbb842abec7941461ea3a13b604c2ef9e
Charles Watkins <chris17453@gmail.com>
2020-07-01 19:30:07 -0400

    Commiting Build 1.2.507

--- source/code/execute.c
+++ source/code/execute.c
@@ -625,9 +625,9 @@ long return_match(cursor_t *cursor,select_t *select,int set){
     
     data_set_t *data_set=cursor->source[set];
 
-    for(long row=0;row<length;row++){
+    for(long row=0;row<10;row++){
         if(row%100==0) {
-        printf ("SET %d %ld of %ld count %d\n",set,row,length,cursor->source_count); 
+            printf ("SET %d %ld of %ld count %d\n",set,row,length,cursor->source_count); 
         }
         results+=evaluate_expressions(cursor,expr);
         data_set->rows[row];
