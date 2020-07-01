688a891145f79eeb96c68022d8c5a1fc9c0bce6f
Charles Watkins <chris17453@gmail.com>
2020-07-01 13:39:19 -0400

    Commiting Build 1.2.479

--- source/code/execute.c
+++ source/code/execute.c
@@ -567,7 +567,7 @@ int execute_select(cursor_t * cursor,select_t *select){
                         continue;
                     } 
 
-                    join_t *join=select->join[set-1];
+                    join_t *join=&select->join[set-1];
                     expressions=join->expression;
                     //results=evaluate_expressions(cursor,expressions);
                     results=1;
