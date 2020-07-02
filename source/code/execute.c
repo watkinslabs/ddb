8d053d0d66111ec1a6f447cb2f82184d7281619a
Charles Watkins <chris17453@gmail.com>
2020-07-01 20:09:26 -0400

    Commiting Build 1.2.527

--- source/code/execute.c
+++ source/code/execute.c
@@ -636,7 +636,7 @@ long return_match(cursor_t *cursor,select_t *select,int set){
         match[row]=evaluate_expressions(cursor,expr);
         data_set->rows[row];
     }
-    if(set+2<cursor->source_count) {
+    if(set+1<cursor->source_count) {
         for(long row=0;row<2;row++){
             results+=return_match(cursor,select,set+1);
         }
