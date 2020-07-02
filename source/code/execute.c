bc09e182c8fd5d22f142ff8de2e0e41907cb80a8
Charles Watkins <chris17453@gmail.com>
2020-07-01 22:11:41 -0400

    Commiting Build 1.2.559

--- source/code/execute.c
+++ source/code/execute.c
@@ -683,7 +683,7 @@ long return_match(cursor_t *cursor,select_t *select,int set){
 int loop=0;
 int eval_row_set(cursor_t *cursor) {
     ++loop;
-    loop%=100;
+    loop%=1000;
     if(loop==0){
         for(int s=0;s<cursor->source_count;s++) {
             printf("%ld-",cursor->source[s]->position);
