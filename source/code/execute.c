f3165f862a563f3bea6607e5a230d493c1711b3c
Charles Watkins <chris17453@gmail.com>
2020-07-05 21:18:42 -0400

    Commiting Build 1.2.635

--- source/code/execute.c
+++ source/code/execute.c
@@ -142,9 +142,13 @@ expression_value_t *evaluate_expression(cursor_t *cursor,expression_t **expr){
         switch(temp_expr->mode){
             // identifier (lets just make this a token code)
             case 1: 
-                    //if(temp_expr->identifier==0) {
-                    ////    printf ("NO IDENT\n");
-                    //}
+                    if(temp_expr->identifier==0) {
+                        if(exprV) free(exprV);
+                        if(tempV) free(tempV);
+                        *expr=temp_expr;
+                        return 0;
+                    }
+                    
                     char *value=get_value_at(cursor,temp_expr->identifier);
                     //debug_identifier(temp_expr->identifier);
                     //type=TOKEN_IDENTIFIER;
