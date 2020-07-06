ad6b168d98536ddcbb51288e1f28e572c7c78c1f
Charles Watkins <chris17453@gmail.com>
2020-07-05 21:07:38 -0400

    Commiting Build 1.2.615

--- source/code/execute.c
+++ source/code/execute.c
@@ -141,7 +141,7 @@ expression_value_t *evaluate_expression(cursor_t *cursor,expression_t **expr){
                 // process token(s)/data point.. 
         switch(temp_expr->mode){
             // identifier (lets just make this a token code)
-            case 1: char *value=get_value_at(cursor,temp_expr->identifier);
+            case 1: char *value=get_value_at(cursor,0);
                     //type=TOKEN_IDENTIFIER;
                     //exprV=
                     tempV=safe_malloc(sizeof(expression_value_t),1);
