3c310de513ee49c479abc4793f90601ea67b8b6f
Charles Watkins <chris17453@gmail.com>
2020-07-08 11:52:59 -0400

    Commiting Build 1.3.60

--- source/code/execute.c
+++ source/code/execute.c
@@ -87,7 +87,8 @@ char *get_value_at(cursor_t *cursor,identifier_t *ident){
                 //ok we know what source/position to look at.. fetch the data and return
                 data_set_t *data_set=cursor->source[ident_lookup.source];
                 //grab the curent position from the cursor.. (saved in dataset)
-                int row_index=data_set->position;
+                long row_index=data_set->position;
+                if (row_index==-1) return DATA_NULL;
                 row_t *row=data_set->rows[row_index];
                 //is it a valid row...
                 if(row_index>=0 && row_index<data_set->row_length) {
