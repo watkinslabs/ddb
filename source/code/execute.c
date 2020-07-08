8d7c4cb8906645046ba97ff7fab82199d2fe6586
Charles Watkins <chris17453@gmail.com>
2020-07-08 13:17:36 -0400

    Commiting Build 1.3.164

--- source/code/execute.c
+++ source/code/execute.c
@@ -742,7 +742,7 @@ long return_match(cursor_t *cursor,select_t *select,int set){
     return results;
 }
 
-int loop=0;
+int loop=1;
 int eval_row_set(cursor_t *cursor,select_t *select) {
     
     //return 0;
