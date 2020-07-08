1be0f87b8c8c9164a794f4616c8d142b9dfa1b30
Charles Watkins <chris17453@gmail.com>
2020-07-07 22:17:41 -0400

    Commiting Build 1.2.886

--- source/code/execute.c
+++ source/code/execute.c
@@ -717,7 +717,7 @@ int loop=1;
 int eval_row_set(cursor_t *cursor,select_t *select) {
    for(int i=0;i<cursor->source_count;i++) if(cursor->source[i]->success==-2) return 0;
     
-
+    return 0;
      //++loop;
     //loop%=1001;
     
