be3ea1b0fd1f6d24d4d74c251cdcad08c90d9566
Charles Watkins <chris17453@gmail.com>
2020-07-08 10:58:33 -0400

    Commiting Build 1.2.980

--- source/code/execute.c
+++ source/code/execute.c
@@ -693,26 +693,28 @@ long return_match(cursor_t *cursor,select_t *select,int set){
         //if(last_join==1) printf("LAST JOIN\n");
 
 
-        if(res==1 && last_join){
-            // the where go's last
-            if(select->where){
-                res=evaluate_expressions(cursor,select->where);
-               printf ("WHERE %d\n",res);
-               // matches+=res;
+        if(res==1){
+            if(last_join){
+                // the where go's last
+                if(select->where){
+                    res=evaluate_expressions(cursor,select->where);
+                printf ("WHERE %d\n",res);
+                // matches+=res;
 
-                if(!res) {
-                    for(int s=set;s<cursor->source_count;s++) {
-                        cursor->source[s]->success=-2;
+                    if(!res) {
+                        for(int s=set;s<cursor->source_count;s++) {
+                            cursor->source[s]->success=-2;
+                        }
+                    } else {
+                        cursor->source[0]->success=1;
                     }
-                } else {
-                    cursor->source[0]->success=1;
                 }
-            }
-            //ok we have an exact filter.. eval the row        
-            eval_row_set(cursor,select);
-        }  else {
-            return_match(cursor,select,set+1);
-        }
+                //ok we have an exact filter.. eval the row        
+                eval_row_set(cursor,select);
+            }  else {
+                return_match(cursor,select,set+1);
+            }//end last join
+        }//end res
     }
     if(matches==0 && last_join==0) return_match(cursor,select,set+1);
     return results;
