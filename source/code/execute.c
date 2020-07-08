b1cc9bfdba0d28a98b852ae1368ff165efadefbd
Charles Watkins <chris17453@gmail.com>
2020-07-08 10:14:06 -0400

    Commiting Build 1.2.960

--- source/code/execute.c
+++ source/code/execute.c
@@ -714,7 +714,7 @@ long return_match(cursor_t *cursor,select_t *select,int set){
 
 int loop=0;
 int eval_row_set(cursor_t *cursor,select_t *select) {
-   for(int i=0;i<cursor->source_count;i++) if(cursor->source[i]->success==-2) return 0;
+   for(int i=0;i<cursor->source_count;i++) if(cursor->source[i]->success<-1) return 0;
     
     //return 0;
      //++loop;
