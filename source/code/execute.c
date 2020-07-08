d41112c94211519266bf9d62ac3f798ea5f2e462
Charles Watkins <chris17453@gmail.com>
2020-07-08 12:26:59 -0400

    Commiting Build 1.3.106

--- source/code/execute.c
+++ source/code/execute.c
@@ -713,7 +713,7 @@ long return_match(cursor_t *cursor,select_t *select,int set){
     }
     if(matches==0 && last_join==0) return_match(cursor,select,set+1);
     if(matches==0 && last_join==1) {
-        cursor->source[set]->success=-1;
+        //cursor->source[set]->success=-1;
 //        printf("BUB\n");
         // the where go's last
         if(select->where){
