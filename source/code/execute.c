6ea8874dc013c144ce183ce853bb8333210e458c
Charles Watkins <chris17453@gmail.com>
2020-07-08 12:32:42 -0400

    Commiting Build 1.3.114

--- source/code/execute.c
+++ source/code/execute.c
@@ -716,6 +716,7 @@ long return_match(cursor_t *cursor,select_t *select,int set){
             }//end last join
         }//end res
     }
+   /*
     if(matches==0 && last_join==0) return_match(cursor,select,set+1);
     if(matches==0 && last_join==1) {
         //cursor->source[set]->success=-1;
@@ -737,6 +738,7 @@ long return_match(cursor_t *cursor,select_t *select,int set){
         //ok we have an exact filter.. eval the row        
         eval_row_set(cursor,select);
     }
+    */
     return results;
 }
 
