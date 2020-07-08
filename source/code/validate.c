9bbb1ed5e9b21aee2d967f897f51e041c7e6fe91
Charles Watkins <chris17453@gmail.com>
2020-07-07 20:08:11 -0400

    Commiting Build 1.2.803

--- source/code/validate.c
+++ source/code/validate.c
@@ -396,18 +396,8 @@ int validate_select(cursor_t * cursor,select_t *select){
             
         }
     }
-    tmp_ptr=select->columns;
-    while(tmp_ptr){
-        // we only care about data sourced from tables
-        if (tmp_ptr->type==TOKEN_IDENTIFIER) {
-            identifier_t *sel_ident=(identifier_t*)tmp_ptr->object;
-            printf("HI\n");
-            debug_identifier(sel_ident);
-        }
-        tmp_ptr=tmp_ptr->next;
-    }
-
-    // validate from and join sources exist
+    
+// validate from and join sources exist
     if(select->from) {
         table_def_t *table_ptr=0;
         table_ptr=get_table_by_identifier(cursor,select->from);
@@ -429,7 +419,16 @@ int validate_select(cursor_t * cursor,select_t *select){
             }
         }
     }
-
+tmp_ptr=select->columns;
+    while(tmp_ptr){
+        // we only care about data sourced from tables
+        if (tmp_ptr->type==TOKEN_IDENTIFIER) {
+            identifier_t *sel_ident=(identifier_t*)tmp_ptr->object;
+            printf("HI\n");
+            debug_identifier(sel_ident);
+        }
+        tmp_ptr=tmp_ptr->next;
+    }
     // at this point. 
     // all FROM/JOIN sources exist and are unique
     // all select columns are UNIQUE
