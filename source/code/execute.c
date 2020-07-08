542a4455cdc15756ee5037a9ce71af7f81dab116
Charles Watkins <chris17453@gmail.com>
2020-07-08 12:24:58 -0400

    Commiting Build 1.3.102

--- source/code/execute.c
+++ source/code/execute.c
@@ -678,7 +678,7 @@ long return_match(cursor_t *cursor,select_t *select,int set){
                                                 for(int s=set;s<cursor->source_count;s++) {
                                                     cursor->source[s]->success=-2;
                                                 }
-                                                //last_join=1;
+                                                last_join=1;
                                             } else {
                                                 cursor->source[set]->success=1;
                                             }
